3577612a8b8c27e338f4e7fda07f341f26110512
Marcsiq2 <marcsiquierpenyafort@gmail.com>
2017-02-26 18:57:49 +0100

    Results benetos

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -9,13 +9,14 @@ import os
 
 
 def compare(y_pred, y, threshold):
+	yy = y_pred
 	score = 0
 	for f in y:
-		for k, ff in enumerate(y_pred):
+		for k, ff in enumerate(yy):
 			diff = abs(float(ff) - float(f))
 			if diff < float(f)*threshold:
 				score += 1
-				y_pred.pop(k)
+				yy.pop(k)
 	return score
 
 def octave_compare(y_pred, y, threshold):
@@ -99,12 +100,16 @@ def klapuri():
 		prec +=p
 		rec += r
 
+	precision = (score/float(prec) if prec else 0)
+	recall = (score/float(rec) if rec else 0)
+	fscore = 2*recall*precision/(recall+precision)
 	print "******************************************"
 	print "Number of correct predicted:\t%i" % (score)	
 	print "Number of predicted:\t\t%i" % (prec)
 	print "Number of Ground Truth:\t\t%i" % (rec)
-	print "Precision:\t\t\t%.4f" % (score/float(prec) if prec else 0)
-	print "Recall:\t\t\t\t%.4f" % (score/float(rec) if rec else 0)	
+	print "Precision:\t\t\t%.4f" % precision
+	print "Recall:\t\t\t\t%.4f" % recall
+	print "F-score:\t\t\t%.4f" % fscore
 
 
 def evaluate_ben(file_location, benetos_location):
@@ -117,6 +122,7 @@ def evaluate_ben(file_location, benetos_location):
 	with open(benetos_location) as f:
 		reader_ben = csv.reader(f, delimiter = "\t")
 		d = list(reader_ben)
+
 	results_ben = []
 	for line in d:
 		b = np.array(filter (lambda a: a!= 0.0, line[1:]))
@@ -126,22 +132,15 @@ def evaluate_ben(file_location, benetos_location):
 	score = 0
 	prec = 0
 	rec = 0
-	i = 1
+	i = 0
 	for k, frame in enumerate(results_GT):
-		try:
-			if k%2:
-				score += octave_compare(frame, results_ben[k], 0.03)
-				#score += compare(frame, results_ben[i], 0.03)
-				prec += len(results_ben[i])
-				rec += len(frame)
-				i += 1
-		except:
-			pass
-	#print "Number of correct predicted:\t%i" % (score)	
-	#print "Number of predicted:\t\t%i" % (prec)
-	#print "Number of Ground Truth:\t\t%i" % (rec)
-	#print "Precision:\t\t\t%.4f" % (score/float(prec) if prec else 0)
-	#print "Recall:\t\t\t\t%.4f" % (score/float(rec) if rec else 0)	
+		if k%2:
+			prec += len(results_ben[i])
+			rec += len(frame)
+			#score += octave_compare(frame, results_ben[k], 0.03)
+			score += compare(frame, results_ben[i], 0.03)
+			i += 1
+
 
 	return score, prec, rec
 
@@ -150,21 +149,27 @@ def benetos():
 	score = 0
 	prec = 0
 	rec = 0
+	print len(filenames)
 	for path, fname in filenames:
-		print "Evaluating " + fname
+		#print "Evaluating " + fname
 		GT_location = path + "/" + fname
-		benetos_location = 'Maps_benetos/' + fname
+		benetos_location = 'Maps_benetos/th_0.001/' + fname
 		s,p,r = evaluate_ben(GT_location, benetos_location)
 		score +=s
 		prec +=p
 		rec += r
 
+
+	precision = (score/float(prec) if prec else 0)
+	recall = (score/float(rec) if rec else 0)
+	fscore = 2*recall*precision/(recall+precision)
 	print "******************************************"
 	print "Number of correct predicted:\t%i" % (score)	
 	print "Number of predicted:\t\t%i" % (prec)
 	print "Number of Ground Truth:\t\t%i" % (rec)
-	print "Precision:\t\t\t%.4f" % (score/float(prec) if prec else 0)
-	print "Recall:\t\t\t\t%.4f" % (score/float(rec) if rec else 0)	
+	print "Precision:\t\t\t%.4f" % precision
+	print "Recall:\t\t\t\t%.4f" % recall
+	print "F-score:\t\t\t%.4f" % fscore
 
 
 def main():
